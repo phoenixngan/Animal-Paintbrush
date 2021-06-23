7bf83e530afc8ffed4e5476b9ab586c24eb8ac08
Phoenix Ngan <phoenix@Phoenixs-MacBook-Pro.local>
2021-06-23 15:28:20 -0700

    brush/images

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
-let colorlist = ['gold', 'yellow', 'turquoise', 'red']
+var imgs = [];
+var d = -1;
+let img;
+// ['./assets/bird.png', './assets/cat1.png', './assets/cat2.png', './assets/dog.png',
+// './assets/dolphin.png', './assets/elephant.png', './assets/giraffe.png', './assets/hedgehog.png',
+// './assets/octopus.png', './assets/otter.png', './assets/owl.png', './assets/penguin.png',
+// './assets/pig.png', './assets/turtle.png', './assets/whale.png']
+function preload(){
+  for(var i = 0; i < 2; i++){
+      imgs[i] = loadImage('./assets/cat' + (i+1) + '.png');
+  }
+  // for (var i = 1; i < 4; i++) {
+  //   imgs[i] = loadImage("./assets/cat"+i+".png");
+  // }
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-    background(255);
+  background(255);
 }
 
 function draw() {
-  noStroke();
-  fill(random(colorlist));
-  ellipse(mouseX, mouseY, 25, 25);
+  //noStroke();
+  if (mouseClicked()) {
+    d++;
+  }
+  if(d = imgs.length+1){
+    d = 0;
+  }
+  console.log(d);
+  let rateNum = 5;
+  if(frameCount % rateNum == 0){
+    image(imgs[d], mouseX, mouseY);
+  }
+  // for (var i=0; i<5; i++) {
+  //   image(imgs[i], mouseX, mouseY);
+  // }
+
+  // let rateNum = 3;
+  // if(frameCount % rateNum == 0){
+  //   let img = loadImage(imgList[frameCount/rateNum % imgList.length]);
+  //   image(img, mouseX, mouseY, 50, 50);
 }
